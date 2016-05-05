08c38f165667e7fe67095f427cc3931bfd0a3050
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2016-05-05 19:43:03 +0530

    Geolocation based demo app

--- Aware App/ViewController.swift
+++ Aware App/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  Aware App
 //
@@ -7,12 +7,80 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
 
+    @IBOutlet weak var latitude: UILabel!
+    @IBOutlet weak var longitude: UILabel!
+    @IBOutlet weak var speed: UILabel!
+    @IBOutlet weak var course: UILabel!
+    @IBOutlet weak var altitude: UILabel!
+    @IBOutlet weak var address: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location : CLLocation = locations[0]
+        
+        self.latitude.text = String(location.coordinate.latitude)
+        self.longitude.text = String(location.coordinate.longitude)
+        self.speed.text = String(location.speed) + "  MPH"
+        self.course.text = String(location.course)
+        self.altitude.text = String(location.altitude)
+        
+        CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) -> Void in
+            
+            var addressString = ""
+            
+            if ((error) != nil){
+                
+                print(error)
+                
+            } else {
+                
+                if let subThoroughfare = placemarks?[0].subThoroughfare {
+                    
+                    addressString = addressString + "\(subThoroughfare)  "
+                    
+                }
+                if let thoroughfare = placemarks?[0].thoroughfare {
+                
+                    addressString = addressString + "\(thoroughfare)\n"
+                    
+                }
+                if let subAdministrativeArea = placemarks?[0].subAdministrativeArea {
+                    
+                    addressString = addressString + "\(subAdministrativeArea)  "
+                    
+                }
+                if let administrativeArea = placemarks?[0].administrativeArea {
+                    
+                    addressString = addressString + "\(administrativeArea)  "
+                    
+                }
+                if let country = placemarks?[0].country {
+                    
+                    addressString = addressString + "\(country)"
+                    
+                }
+                self.address.text = addressString
+                
+            }
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
